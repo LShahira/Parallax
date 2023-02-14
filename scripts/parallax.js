2e232676f94bcaf36cd0458bdde4f8f2468f2d2d
LShahira <shahiralabeeba@gmail.com>
2023-02-14 17:28:56 -0600

    home page done

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -4,3 +4,17 @@ let mountains_behind= document.getElementById('mountains_behind');
 let text = document.getElementById('text');
 let btn = document.getElementById('btn');
 let mountains_front= document.getElementById('mountains_front');
+let header = document.querySelector('header');
+
+// it MOVES?!?! the stars!?!? 
+window.addEventListener('scroll',function(){
+    let value = window.scrollY; //when scrolling up down
+    stars.style.left=value *0.5 + 'px'; // stars moves x axis!?!?
+    moon.style.top=value * 1.5 + 'px'; 
+    mountains_behind.style.top=value * 0.5 + 'px'; 
+    mountains_front.style.top=value * 0 + 'px'; 
+    text.style.marginRight=value * 4 + 'px'; 
+    text.style.marginTop=value * 1.5 + 'px'; 
+    btn.style.marginTop=value * 1.5 + 'px'; 
+    header.style.top=value * 0.5 + 'px'; 
+})
