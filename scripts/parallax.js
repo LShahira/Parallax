36c8d7701e2473caf00c99fc876160a59a9305ad
LShahira <shahiralabeeba@gmail.com>
2023-02-14 16:57:03 -0600

    animation things w js

---
+++ scripts/parallax.js
@@ -0,0 +1,6 @@
+let stars= document.getElementById('stars');
+let moon= document.getElementById('moon');
+let mountains_behind= document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front= document.getElementById('mountains_front');
